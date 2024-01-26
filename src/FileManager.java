e6da267391f75eec5f6a545d2e884d17bf85a6df
rvdxk <radek.szczot@gmail.com>
2024-01-26 17:21:14 +0100

    Refreshed code

--- src/FileManager.java
+++ src/FileManager.java
@@ -5,25 +5,25 @@ import java.util.stream.Stream;
 
 public class FileManager
 {
-    public void listFilesInDirectory(String directory) //wyświetlenie listy plików
+    public void listFilesInDirectory(String directory)
     {
         Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
-                //zwracanie tablicy i pobranie zawartości
-                .filter(File::isFile) //przefiltrowanie by tablica zawierała tylko pliki
-                .map(File::getName) //pobranie nazw plików
+
+                .filter(File::isFile)
+                .map(File::getName)
                 .forEach(System.out::println);
     }
 
-    public void sortFilesInDirectory(String directory) //wyświetlenie posortowanej listy plików
+    public void sortFilesInDirectory(String directory)
     {
 
         Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
                 .filter(File::isFile)
-                .sorted((o1, o2) -> Long.compare(o2.length(), o1.length())) //sortowanie wielkości -> malejąco
+                .sorted((o1, o2) -> Long.compare(o2.length(), o1.length()))
                 .forEach(file ->
                 {
-                    System.out.println(file.getName()); //nazwa
-                    System.out.println(file.length()); //wielkość
+                    System.out.println(file.getName());
+                    System.out.println(file.length());
                     System.out.println();
                 });
     }
